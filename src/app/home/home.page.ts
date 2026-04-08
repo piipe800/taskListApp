5be3c2e96710e42b96503e2331b6f690d9580407
Felipe Leon <piipe_80@hotmail.com>
2026-04-08 16:34:09 -0500

    feature: Se implemento la gestion de categorias, con funcionalidades agregar y eliminar.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { Task } from '../models/task';
-import { TaskService } from '../services/task.service';
+import { TaskService } from '../services/task/task.service';
 import { ReactiveFormsModule, FormControl } from '@angular/forms';
+import { CategoryService } from '../services/category/category.service';
+import { Category } from '../models/category';
 
 @Component({
   selector: 'app-home',
@@ -14,22 +16,32 @@ export class HomePage implements OnInit {
   
   tasks: Task[] = [];
   newTaskTitle = new FormControl('');
+  categories: Category[] = [];
+  selectedCategoryControl = new FormControl<string | null>(null);
+  newCategoryControl = new FormControl('');
 
-  constructor(private taskService: TaskService) {}
+  constructor(
+    private taskService: TaskService,
+    private categoryService: CategoryService) {}
 
   ngOnInit() {
     this.loadTasks();
+    this.loadCategories();
   }
 
   loadTasks() {
     this.tasks = this.taskService.getTasks();
   }
 
+  loadCategories() {
+    this.categories = this.categoryService.getCategories();
+  }
+
   addTask() {
     const value = this.newTaskTitle.value;
 
     if (!value?.trim()) return;
-    
+
     this.taskService.addTask(value);
     this.newTaskTitle.reset();
     this.loadTasks();
@@ -43,5 +55,20 @@ export class HomePage implements OnInit {
   deleteTask(taskId: string) {
     this.taskService.deleteTask(taskId);
     this.loadTasks();
+  }
+
+  addCategory() {
+    const value = this.newCategoryControl.value;
+
+    if (!value?.trim()) return;
+
+    this.categoryService.addCategory(value);
+    this.newCategoryControl.reset();
+    this.loadCategories();
+  }
+
+  deleteCategory(categoryId: string) {
+    this.categoryService.deleteCategory(categoryId);
+    this.loadCategories();
   }
 }
---
+++ src/app/services/category/category.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Category } from 'src/app/models/category';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoryService {
+
+  STORAGE_KEY = 'categories';
+
+  saveCategories(categories: Category[]) {
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(categories));
+  }
+
+  addCategory(name: string) {
+    const categories = this.getCategories();
+
+    const newCategory: Category = {
+      id: new Date().toString(),
+      name,
+    };
+    categories.push(newCategory);
+    this.saveCategories(categories);
+  }
+
+  deleteCategory(categoryId: string) {
+    const categories = this.getCategories().filter(c => c.id !== categoryId);
+    this.saveCategories(categories);
+  }
+  
+  getCategories(): Category[] {
+    const categories = localStorage.getItem(this.STORAGE_KEY);
+    return categories ? JSON.parse(categories) : [];
+  }
+}
