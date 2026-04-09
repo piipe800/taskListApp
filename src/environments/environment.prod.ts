2937a47f35d9093258d942f4ef32071c1de55681
Felipe Leon <piipe_80@hotmail.com>
2026-04-08 23:35:43 -0500

    Se instala capacitor, necesario para build en movil

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+      apiKey: "AIzaSyDZFNLRcSGxeY7bKdSM2N69AN6IW_XC1CY",
+      authDomain: "task-list-app-b84b2.firebaseapp.com",
+      projectId: "task-list-app-b84b2",
+      storageBucket: "task-list-app-b84b2.firebasestorage.app",
+      messagingSenderId: "447479023707",
+      appId: "1:447479023707:web:a96a033e2b1e93caa7977e"
+    }
 };
